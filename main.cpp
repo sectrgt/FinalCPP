bbce3905ee999ed833d26886cc34f728e10e1281
JoeeBayer <Josephbayer29@yahoo.com>
2016-06-13 16:09:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -43,65 +43,6 @@ void GuiClass::GuiPaint()
 
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
-
-
-/* Implements a button which can be clicked on
-            Modified to add default constructor! */
- 
-
-/*class ButtonClass {
-            public:
-            ButtonClass(String Text, int X1,int Y1, int X2, int Y2);
-            /* Creates a button with upper left corner at X1,Y1 and lower
-            right corner at X2,Y2 with Text centered in box */
-            ButtonClass();
-            void SetButton(String Text, int X1,int Y1, int X2, int Y2);
-            void Paint();
-            bool IsHit(int x, int y);
-            /* Returns true if and only if (x,y) is on the button */
-            private:
-                        int MyX1, MyY1, MyX2, MyY2;
-                        String MyText;
-            }; 
-//-------------------------------------------------------------------
-ButtonClass::ButtonClass()
-{}
-//-------------------------------------------------------------------
-ButtonClass::ButtonClass(String Text, int X1,int Y1, int X2, int Y2):
-            MyText(Text), MyX1(X1), MyY1(Y1), MyX2(X2), MyY2(Y2)
-/* Creates a button with upper left corner at X1,Y1 and lower
-            right corner at X2,Y2 with Text centered in box */
-{}
-//-------------------------------------------------------------------
-void ButtonClass::SetButton(String Text, int X1,int Y1, int X2, int Y2)
-/* Sets button with upper left corner at X1,Y1 and lower
-            right corner at X2,Y2 with Text centered in box */
-{
-            MyText = Text;
-            MyX1 = X1;
-            MyY1 = Y1;
-            MyX2 = X2;
-            MyY2 = Y2;
-}
-//-------------------------------------------------------------------
-void ButtonClass::Paint()
-{
-            SetColor(BLACK);
-   SetThickness(2);
-            Rectangle(MyX1,MyY1,MyX2,MyY2);
-            gotoxy((MyX1+MyX2)/2, 5+(MyY1+MyY2)/2);
-            DrawCenteredText(MyText);
-}
-//-------------------------------------------------------------------
-bool ButtonClass::IsHit(int x, int y)
-/* Returns true if and only if point (x,y) is on the button */
-{
-            return (x >= MyX1 && x <= MyX2 && y >= MyY1 && y <= MyY2);
-}
-//-------------------------------------------------------------------
-//-------------------------------------------------------------------
-//-------------------------------------------------------------------
-//=------------------------------------------------TEMP
 #include <lvp\vector.h>
 
 class PanelClass
@@ -110,9 +51,6 @@ class PanelClass
 		DisplayPanel(int lr, double value, int ystart, int xstart);
 		DelPanel();
 	private: 
-
-
-
 };
 PanelClass::DisplayPanel(int lr, double value, int ystart, int xstart)//panel display function
 {
